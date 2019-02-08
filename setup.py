a05328f3da676997d95eef7065a2c9543ecb032c
Nobody912 <erikji123@yahoo.com>
2019-02-08 12:38:17 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='PyRSA',
+    python_requires='>3',
+    version='0.1',
+    description='A simple implementation of PyCryptodome for RSA encryption in a CLI interface.',
+    url='https://github.com/Nobody912/PyRSA',
+    author='Erik Ji',
+    author_email='erikji@tuta.io',
+    license='MIT',
+    dependency_links=[
+        'https://github.com/pwaller/pyfiglet/tarball/0.7.3'
+    ],
+    install_requires=[
+        'termcolor',
+        'pycryptodome',
+    ],
+    zip_safe=False)

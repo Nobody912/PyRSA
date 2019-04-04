5bd2a6d2cfc6ef34dd741aa78da1d71699ac88ab
Nobody912 <erikji123@yahoo.com>
2019-04-04 11:11:26 -0700

    Update pyrsa.py

--- src/pyrsa/pyrsa.py
+++ src/pyrsa/pyrsa.py
@@ -5,7 +5,7 @@ import sys
 import os
 import time
 
-# from Crypto.PublicKey import RSA
+from Crypto.PublicKey import RSA
 from Crypto.Random import get_random_bytes
 from Crypto.Cipher import AES, PKCS1_OAEP
 import pyfiglet
